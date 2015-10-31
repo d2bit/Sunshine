5b590baf44c3742a35597079c2f1cbb3ca8df90f
David BF <burretfresket@gmail.com>
2015-10-31 10:49:34 +0100

    Add fake forecast data

--- app/src/main/java/net/d2bit/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/net/d2bit/sunshine/app/MainActivityFragment.java
@@ -6,6 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -17,6 +21,16 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] data = {
+                "Today - Sunny - 31/17",
+                "Tomorrow - Foggy - 21/8",
+                "Weds - Cloudy - 22/17",
+                "Thurs - Rainy - 18/11",
+                "Fri - Foggy - 21/10",
+                "Sat - Cloudy - 23/18"
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
