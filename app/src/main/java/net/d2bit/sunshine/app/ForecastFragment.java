12d161d11a889286d6ab0387b34a27e2205d974c
David BF <burretfresket@gmail.com>
2015-11-08 19:06:23 +0100

    Add Temperature units preference

--- app/src/main/java/net/d2bit/sunshine/app/ForecastFragment.java
+++ app/src/main/java/net/d2bit/sunshine/app/ForecastFragment.java
@@ -217,6 +217,15 @@ public class ForecastFragment extends Fragment {
          * Prepare the weather high/lows for presentation.
          */
         private String formatHighLows(double high, double low) {
+            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String defaultTempUnit = getString(R.string.pref_temperature_default);
+            String tempUnit = sharedPref.getString(getString(R.string.pref_temperature_key), defaultTempUnit);
+
+            if (!tempUnit.equals(defaultTempUnit)) {
+                high = high * 1.8 + 32;
+                low = low * 1.8 + 32;
+            }
+
             // For presentation, assume the user doesn't care about tenths of a degree.
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
