f0899060f6ab2be052acc78276ebdc169fb95461
David BF <burretfresket@gmail.com>
2015-11-08 16:39:54 +0100

    Open SettingsActivity from the Settings button on the menu
    * From MainActivity & DetailActivity

--- app/src/main/java/net/d2bit/sunshine/app/MainActivity.java
+++ app/src/main/java/net/d2bit/sunshine/app/MainActivity.java
@@ -1,5 +1,6 @@
 package net.d2bit.sunshine.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -44,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent preferencesIntent = new Intent(this, SettingsActivity.class);
+            startActivity(preferencesIntent);
             return true;
         }
 
