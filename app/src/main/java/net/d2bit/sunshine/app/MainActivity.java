8dc42f6ea41c7d118ff865929ac3a56cc88db565
David BF <burretfresket@gmail.com>
2015-11-17 20:56:03 +0100

    Launch a Map intent to show the location

--- app/src/main/java/net/d2bit/sunshine/app/MainActivity.java
+++ app/src/main/java/net/d2bit/sunshine/app/MainActivity.java
@@ -1,7 +1,10 @@
 package net.d2bit.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -45,11 +48,34 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             Intent preferencesIntent = new Intent(this, SettingsActivity.class);
             startActivity(preferencesIntent);
             return true;
         }
+        if (id == R.id.action_map) {
+            showMap();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showMap() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String location = prefs.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
