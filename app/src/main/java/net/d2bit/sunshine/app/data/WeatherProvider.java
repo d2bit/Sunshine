fd945ebc5804689e3ba3d7ec0b5f6801a56a0264
David BF <burretfresket@gmail.com>
2015-11-28 10:04:16 +0100

    Build URI matcher

--- app/src/main/java/net/d2bit/sunshine/app/data/WeatherProvider.java
+++ app/src/main/java/net/d2bit/sunshine/app/data/WeatherProvider.java
@@ -117,14 +117,19 @@ public class WeatherProvider extends ContentProvider {
     static UriMatcher buildUriMatcher() {
         // 1) The code passed into the constructor represents the code to return for the root
         // URI.  It's common to use NO_MATCH as the code for this case. Add the constructor below.
-
+        UriMatcher sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        String authority = WeatherContract.CONTENT_AUTHORITY;
 
         // 2) Use the addURI function to match each of the types.  Use the constants from
         // WeatherContract to help define the types to the UriMatcher.
+        sURIMatcher.addURI(authority, WeatherContract.PATH_WEATHER, WEATHER);
+        sURIMatcher.addURI(authority, WeatherContract.PATH_WEATHER + "/*", WEATHER_WITH_LOCATION);
+        sURIMatcher.addURI(authority, WeatherContract.PATH_WEATHER + "/*/#", WEATHER_WITH_LOCATION_AND_DATE);
+        sURIMatcher.addURI(authority, WeatherContract.PATH_LOCATION, LOCATION);
 
 
         // 3) Return the new matcher!
-        return null;
+        return sURIMatcher;
     }
 
     /*
